97747bda4db6b32c2c22a01786dc7711a791656b
jhsay0314 <jhsay0314@gmail.com>
2024-06-18 02:42:50 +0900

    CI/CD APPLY

--- src/main/java/CyPirates/avere/global/jwt/JwtUtil.java
+++ src/main/java/CyPirates/avere/global/jwt/JwtUtil.java
@@ -3,13 +3,16 @@ package CyPirates.avere.global.jwt;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTVerificationException;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
 
 @Component
 public class JwtUtil {
-    private static final String SECRET = "cyberpirates1999and1997";
+    @Value("${spring.jwt.secret}")
+    private String SECRET;
+
     private static final long EXPIRATION_TIME = 86400000;
 
     public String generateToken(String username) {
--- src/test/java/CyPirates/avere/AvereApplicationTests.java
+++ src/test/java/CyPirates/avere/AvereApplicationTests.java
@@ -1,10 +1,16 @@
 package CyPirates.avere;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
 @SpringBootTest
 class AvereApplicationTests {
+
+	@MockBean
+	private UserDetailsService userDetailsService;
 
 	@Test
 	void contextLoads() {
