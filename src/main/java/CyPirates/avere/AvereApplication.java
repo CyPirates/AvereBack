b0eb0841003ae225c9dfea86c77007f6da14187b
jhsay0314 <jhsay0314@gmail.com>
2024-06-28 05:01:52 +0900

    fix)swagger https

--- src/main/java/CyPirates/avere/AvereApplication.java
+++ src/main/java/CyPirates/avere/AvereApplication.java
@@ -1,8 +1,13 @@
 package CyPirates.avere;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(servers = {
+		@io.swagger.v3.oas.annotations.servers.Server(url = "http://localhost:8080", description = "Local Server"),
+		@io.swagger.v3.oas.annotations.servers.Server(url = "https://avere.cypirates.xyz", description = "Production Server")
+})
 @SpringBootApplication
 public class AvereApplication {
 
